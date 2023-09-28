f3eb0d791ccbec046b11d2822a579075cf7f2493
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-28 20:10:48 +0200

    remove static widget

--- src/ipymolecularnodes/__init__.py
+++ src/ipymolecularnodes/__init__.py
@@ -10,27 +10,6 @@ try:
 except importlib.metadata.PackageNotFoundError:
     __version__ = "unknown"
 
-class BlenderWidget(anywidget.AnyWidget):
-    label = Unicode("Color: ").tag(sync=True)
-    base64Image = Unicode(
-        "data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
-    ).tag(sync=True)
-
-
-    count = Int(0).tag(sync=True)
-    svalue = Float(30).tag(sync=True)
-
-    @observe("svalue")
-    def _observe_svalue(self, change):
-        self.base64Image = render_to_image(self.count, self.svalue)
-
-    @observe("count")
-    def _observe_count(self, change):
-        self.base64Image = render_to_image(self.count, self.svalue)
-
-    _esm = pathlib.Path(__file__).parent / "static" / "widget.js"
-    # _css = pathlib.Path(__file__).parent / "static" / "widget.css"
-
 
 class BlenderInteractiveWidget(anywidget.AnyWidget):
     label = Unicode("Color: ").tag(sync=True)
