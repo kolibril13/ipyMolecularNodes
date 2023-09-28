7d43752ecb2d32f8941b2ee2a3f71e643080c1c1
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2023-09-28 20:39:40 +0200

    simplify interactive example

--- src/ipymolecularnodes/__init__.py
+++ src/ipymolecularnodes/__init__.py
@@ -1,8 +1,7 @@
 import importlib.metadata
 import pathlib
-from .render_to_gltf_model import render_to_gltf_structure
 import anywidget
-from traitlets import Int, Unicode, observe,Float
+from traitlets import Unicode
 
 try:
     __version__ = importlib.metadata.version("ipymolecularnodes")
@@ -11,11 +10,8 @@ except importlib.metadata.PackageNotFoundError:
 
 
 class BlenderInteractiveWidget(anywidget.AnyWidget):
-    label = Unicode("Color: ").tag(sync=True)
 
-    model_data = render_to_gltf_structure(3, 10)
-    torusname = Unicode(model_data).tag(sync=True)
+    torusname = Unicode("").tag(sync=True)
 
     _esm = pathlib.Path(__file__).parent / "static" / "interactive_widget.js"
     # _css = pathlib.Path(__file__).parent / "static" / "widget.css"
-
